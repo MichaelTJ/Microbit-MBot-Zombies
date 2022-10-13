860a79f547c8c23379231fe9cf15c3450bbcbf94
MichaelTJ <michaeltomjensen@gmail.com>
2022-10-13 10:53:59 +1000

    commit test

--- main.js
+++ main.js
@@ -1,41 +1,19 @@
 function scratchCode(){
-    
-    //put your microbit code here
-    //instructions given to mBot through basic.showLeds()
-    //there are wrappers for most functions.
-    
-    radio.setGroup(1);
     radio.onReceivedNumber(function (receivedNumber) {
-        
-        if (500000 > control.deviceSerialNumber()) {
-            basic.showLeds(`
-                # # # # #
-                . . . . .
-                # # # # #
-                . . . . .
-                . . . . .
-                `)
-        }
-        else{
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
+        basic.showLeds(`
+            # # # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
             `)
-        }
     })
+    radio.setGroup(1)
     basic.forever(function () {
         radio.sendNumber(0)
     })
-    basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
+    
+    
 }
 
 
