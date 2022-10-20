52787c212ca3ddfaf6622cd0426eee7d9cdae64b
MichaelTJ <michaeltomjensen@gmail.com>
2022-10-20 11:49:05 +1000

    async await update

--- main.js
+++ main.js
@@ -1,34 +1,54 @@
 function scratchCode(){
     
-    //put your microbit code here
+    //paste your makecode microbit code here
     //instructions given to mBot through basic.showLeds()
-    //there are wrappers for most functions.
+    //unfortunately you have to put the word 'await' before 
+    //  all 'basic.____' functions
+    //Example: 'basic.pause(100)' becomes 'await basic.pause'
+
+    //You will also have to put 'async' in front of function
+    //Example:
+    //radio.onReceivedNumber(function (receivedNumber) {
+    //becomes:
+    //radio.onReceivedNumber(async function (receivedNumber) {
     
+    radio.onReceivedNumber(async function (receivedNumber) {
+        if (control.deviceSerialNumber() < 500000) {
+            await basic.showLeds(`
+                # # # # #
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+            await basic.pause(500)
+            await basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                . . . . .
+                . . . . .
+                `)
+            await basic.pause(500)
+        } else {
+            await basic.showLeds(`
+                # # # # #
+                # # # # #
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+            await basic.pause(500)
+                
+        }
+        
+        radio.sendNumber(0)
+    })
+    radio.setGroup(1)
     basic.forever(async function () {
-        basic.showLeds(`
-                # # # # #
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
-            console.log('before');
-            await basic.pause(3000)
-            basic.showLeds(`
-                # # # # #
-                # # # # #
-                # # # # #
-                . . . . .
-                . . . . .
-                `)
-            console.log('after');
-            await basic.pause(3000)
+        radio.sendNumber(0)
     })
     
-
-
-    return 'leave these last 2 lines';
-
 }
 
 
